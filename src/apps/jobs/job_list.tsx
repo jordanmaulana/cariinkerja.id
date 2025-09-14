193427508ee2f83a723329e89fb7e38728cfaee5
jordan <jordanmaulana26@gmail.com>
2025-09-14 19:51:56 +0700

    lint: run prettier

---
+++ src/apps/jobs/job_list.tsx
@@ -0,0 +1,3 @@
+export default function job_list() {
+  return <div className="pt pt-4"></div>;
+}
---
+++ src/models/jobs.ts
@@ -0,0 +1,12 @@
+export interface Job {
+  uid: string;
+  title: string;
+  companyName: string;
+  location: string;
+  employmentType: string;
+  workLocation: string;
+  jobTitleCategory: string;
+  postedOn: Date;
+  sourcePlatform: string;
+  createdOn: Date;
+}
